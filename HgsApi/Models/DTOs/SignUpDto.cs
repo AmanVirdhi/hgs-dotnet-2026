fe56f74ef0617eee78e4ca1d7460ffaeaf097d23
AMANDEEP VIRDHI <amanvirdhi123@gmail.com>
2026-05-04 01:15:59 +0530

    fix email

--- HgsApi/Models/DTOs/SignUpDto.cs
+++ HgsApi/Models/DTOs/SignUpDto.cs
@@ -1,9 +1,16 @@
-namespace HgsApi.Models
+using System.ComponentModel.DataAnnotations;
+namespace HgsApi.Models.DTOs
 {
     public class SignUpDto
     {
+        [Required]
         public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; } = string.Empty;
+
+        [Required]
         public string Password { get; set; } = string.Empty;
     }
 }
